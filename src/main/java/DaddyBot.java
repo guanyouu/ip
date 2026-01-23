ea03ea35ce460cb9b6ecbe45f9fabce440374437
guanyouu <cgy265@gmail.com>
2026-01-24 01:12:17 +0800

    Skeleton of chat done

---
+++ src/main/java/DaddyBot.java
@@ -0,0 +1,10 @@
+public class DaddyBot {
+    public static void main(String[] args) {
+        System.out.println("___________________________________________________________________\n");
+        System.out.println("what can daddy do for you?");
+        System.out.println("be sure to add 'please daddy' at the end of your input");
+        System.out.println("___________________________________________________________________\n");
+        System.out.println("daddy's gonna go now...");
+        System.out.println("___________________________________________________________________\n");
+    }
+}
